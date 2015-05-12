3d20eb0428f721c5f0d061c1e351ed130589064a
Kareem Francis <francis.kareem@gmail.com>
2015-05-11 23:39:15 -0400

    Added ability to set sort criteria by parsing input text

--- src/lunch_bot/yelp.clj
+++ src/lunch_bot/yelp.clj
@@ -11,6 +11,9 @@
 (def ^:private yelp-client
   (client/create yelp-key yelp-consumer-secret yelp-token yelp-token-secret))
 
+;; Sort mode: 0=Best matched (default), 1=Distance, 2=Highest Rated
+(def ^:private yelp-sort-options {:best 0 :distance 1 :highest-rated 2})
+
 (def ^:private zipcode "07302")
 (def ^:private default-params {:term "restaurants"
                                :location zipcode
@@ -52,11 +55,21 @@
       (or (re-matches #"kilometers*" units) (= units "km")) (* 1000 increment)
       :else nil)))
 
-(defn handle-query-request 
-  ;; command structure -> [sort-by] [category] within [increment] [units] of [location]
+(defn- sort-text-to-mode
+  ;; Convert english sort criteria to yelp sort mode digit
+  [sort-text]
+  (cond
+      (re-matches #"best" sort-text) (yelp-sort-options :best)
+      (re-matches #"closest" sort-text) (yelp-sort-options :distance)
+      (re-matches #"highest.rated" sort-text) (yelp-sort-options :highest-rated)
+      :else nil))
+
+(defn handle-query-request
+  ;; Command structure -> [sort-text] [category] within [increment] [units] of [location]
   [text]
-  (let [[sort-by term increment units location] (rest (re-matches #"(\.+\s)*(\w+) within (\d+) (\w+) of (.+)" text))
-        radius (convert-to-meters increment units)]
+  (let [[sort-text term increment units location] (rest (re-matches #"(\.+\s)*(\w+) within (\d+) (\w+) of (.+)" text))
+        radius (or (convert-to-meters increment units) (default-params :radius_filter))
+        sort-mode (or (sort-text-to-mode sort-text) (default-params :sort))]
     (if (nil? term)
       (str "Unparsable request " text)
-      (get-by-query {:term term :radius_filter radius :location location}))))
+      (get-by-query {:term term :radius_filter radius :location location :sort sort-mode}))))
